0585befadd5715a3bbcb2aabf70ebb397c53874c
Utsav2 <ukshah2@illinois.edu>
2014-11-24 00:58:03 -0600

    changes to ui

--- app.py
+++ app.py
@@ -1,15 +1,13 @@
 import os
 import json
-from flask import Flask, render_template, url_for, jsonify, request
+from flask import Flask, render_template, jsonify, request
 import psycopg2
-import calendar, datetime
+import datetime
+import requests
 
 # initialization
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://ivxreaxdzurffp:b8iMA7KJCGaFMKIjZYhtzqywfm@ec2-184-73-194-196.compute-1.amazonaws.com:5432/d8rilo7dk8mh5i'
-app.config.update(
-    DEBUG = True,
-)
 
 conn = psycopg2.connect(dbname="d8rilo7dk8mh5i", user= "ivxreaxdzurffp", password="b8iMA7KJCGaFMKIjZYhtzqywfm", host="ec2-184-73-194-196.compute-1.amazonaws.com")
 
@@ -41,10 +39,10 @@ def adminBuildSearch():
 @app.route("/admin1", methods=['POST'])
 def adminAccess():
     
-    buildinput = request.form['buildinput'];
-    nameinput = request.form['nameinput'];
-    latinput = request.form['latinput'];
-    longinput = request.form['longinput'];
+    buildinput = request.form['buildinput']
+    nameinput = request.form['nameinput']
+    latinput = request.form['latinput']
+    longinput = request.form['longinput']
     flag = request.form['flag']
 
     SQL = ''
@@ -68,11 +66,9 @@ def admin():
    return render_template('admin.html')
 
 @app.route("/getCourses")
-def getCourseList():  
-  
-  term = request.args['term'];
-
-  SQL = "SELECT crn, title, subjnbr FROM sections WHERE subjnbr LIKE '%%" + term + "%%' or title LIKE '%%" + term + "%%' or crn LIKE '%%" + term + "%%' ;"
+def getCourseList():
+  term = request.args['term'].upper()
+  SQL = "SELECT DISTINCT crn, title, subjnbr FROM sections WHERE subjnbr LIKE '%%" + term + "%%' or title LIKE '%%" + term + "%%' or crn LIKE '%%" + term + "%%' ;"
   data = (term, )
 
   cursor.execute(SQL)
@@ -92,7 +88,7 @@ def getCourseListWithDays():
   
   term = request.args['term'];
 
-  SQL = "SELECT m, t, w, h, f, begintime, endtime, crn, subjnbr, title, latitude, longitude FROM sections, buildings WHERE crn = '" + term + "' AND sections.building = buildings.building;"
+  SQL = "SELECT DISTINCT m, t, w, h, f, begintime, endtime, crn, subjnbr, title, latitude, longitude FROM sections, buildings WHERE crn = '" + term + "' AND sections.building = buildings.building;"
 
   cursor.execute(SQL)
   rows = [x for x in cursor]
@@ -110,7 +106,12 @@ def getCourseListWithDays():
 
   return json.dumps(courses)
 
+@app.route("/directions")
+def directions():
+    url = "http://maps.googleapis.com/maps/api/directions/json?origin=Toronto&destination=Montreal"
+    r = requests.get(url)
+    return  jsonify(r.json())
 # launch
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
--- static/app.js
+++ static/app.js
@@ -1,173 +1,191 @@
 var app = angular.module('cs411ProjectApp', ['ngRoute'])
-
 var map;
-
 var days = {}
-
 days['m'] = [];
-
 days['t'] = [];
-
 days['w'] = [];
-
 days['r'] = [];
-
 days['f'] = [];
 
-var alreadyAddedCourse = {};
-
+var directionsDisplay;
+var directionsService = new google.maps.DirectionsService();
 
 function initialize() {
-    
     var mapOptions = {
       center: { lat: 40.1150, lng: -88.2728},
       zoom: 16
     };
-
 	map = new google.maps.Map(document.getElementById('map-canvas'),
         mapOptions);
+
+	directionsDisplay = new google.maps.DirectionsRenderer();
+	directionsDisplay.setMap(map);
 }
-
 function moveToLocation(lat, lng){
-
     var center = new google.maps.LatLng(lat, lng);
-
     map.panTo(center);
 }
 
+function regexify_search(term){
 
-
+	var whitespace = term.indexOf(' ');
+	if(whitespace != -1){
+		before = term.substring(0, whitespace);
+		after = term.substring(whitespace+1, term.length);
+		if(before.search(/[^a-zA-Z]+/) === -1 && after.search(/^\d+(?:\.\d{1,2})?$/) !== -1){
+			return before + '-' + after;
+		}
+	}
+	return term;
+}
 $( "#search" ).autocomplete({
-
 	source: function (request, response) {
+
+		request.term = regexify_search(request.term);
 	  $.getJSON("/getCourses?term=" + request.term, function (data) {
-
 	      r = [];
-
 	      duplicates = [];
-
 	      var count = 0;
-
 	      for(var i in data){
-
 	        if(!(data[i].crn in duplicates)){
-
 	          r[count] = {
-
 	            label: data[i].subjnbr + ' ' + data[i].title + ' ' + data[i].crn,
-
 	            value: data[i].subjnbr + ' ' + data[i].title,
-
 	            id: data[i].crn
-
 	          }
-
 	          duplicates[data[i].crn] = "1"
-
 	          count++;
-
 	        }
-
 	      }
-
 	      response(r);
 	  });
 	},
-
 	minLength: 2,
 	delay:100,
 	select: function( event, ui ) {
-
 		putOnMap(ui);
-
 	}
 });
+
+var currentMarkers = [];
+current_info_windows = [];
+currently_rendered = 'm';
 
 function putOnMap(data){
 
 	$.getJSON("/getCoursesWithDays?term=" + data.item.id, function(result){
-
-		for(var i in result){
-
-			if(result[i].m != null){
-				days['m'].push(result[i]);
+			if(result[0].m != null){
+				days['m'].push(result[0]);
 			}
-			if(result[i].t != null){
-				days['t'].push(result[i]);
+			if(result[0].t != null){
+				days['t'].push(result[0]);
 			}
-			if(result[i].w != null){
-				days['w'].push(result[i]);
+			if(result[0].w != null){
+				days['w'].push(result[0]);
 			}
-			if(result[i].h != null){
-				days['r'].push(result[i]);
+			if(result[0].h != null){
+				days['r'].push(result[0]);
 			}
-			if(result[i].f != null){
-				days['f'].push(result[i]);
+			if(result[0].f != null){
+				days['f'].push(result[0]);
 			}
-		}
+			renderMap(currently_rendered);
 	});
 
 }
+function clearCourses(day){
+	$('#CourseList').empty();
+	$('#' + day + '-list').empty();
+}
 
-var currentMarkers = [];
+function compareTimes(a, b){
+	return parseInt(a.begintime.substring(0, 2)) > parseInt(b.begintime.substring(0, 2));
+}
 
 function renderMap(day){
-
 	clearMarkers();
-
+	clearCourses(day);
 	for(var i in days[day]){
-
-		var location = new google.maps.LatLng((days[day])[i].latitude, (days[day])[i].longitude);
-
 		var tuple = (days[day])[i];
-
+		var location = new google.maps.LatLng(tuple.latitude, tuple.longitude);
 		var marker = new google.maps.Marker({
-
 			position: location,
 			title: days[day].title,
 			map: map
-
 		});
-
-		var content_string = '<div style="width:200px; height:100px"><h3>' + tuple.subjnbr + '</h3>' + 
+		var content_string = '<div class = "course"><h3>' + tuple.subjnbr + '</h3>' + 
 							 '<p>' + tuple.title + 
 							 '<p><span>' + tuple.begintime +  
 							 ' - ' + tuple.endtime + '</span></div>';
 
-
-
 		var infowindow = new google.maps.InfoWindow({
-
      		content: content_string
-
   		});
-
-  		google.maps.event.addListener(marker, 'click', function() {
-
-    		infowindow.open(map,marker);
-
-  		});
-
+  		(days[day])[i].content_string = content_string;
+  		add_infowindow(marker, infowindow);
   		marker.setMap(map);
-
 		currentMarkers.push(marker);
-
-		map.panTo(location);
+		current_info_windows.push(infowindow);
 	}
+	days[day].sort(compareTimes);
+	appendContentStrings(days[day], day);
+	calcRoute(day);
+	map.panTo(location);
+	currently_rendered = day;
+}
 
+function appendContentStrings(days_array, day){
+	for(var i in days_array){
+		$('#' + day + '-list').append(days_array[i].content_string);
+	}
+}
 
-
-
+function add_infowindow(marker, infowindow){
+	  	google.maps.event.addListener(marker, 'click', function() {
+    		infowindow.open(map,marker);
+  		}); 
 }
 function clearMarkers(){
-
 	for(var i in currentMarkers){
-
 		currentMarkers[i].setMap(null);
 	}
-
 	currentMarkers = [];	
+	current_info_windows = [];
 }
 
+function calcRoute(day) {
+	if(days[day].length <= 1){
+		directionsDisplay.set('directions', null);
+		return;
+	}
+	var origin = new google.maps.LatLng((days[day])[0].latitude, (days[day])[0].longitude);
+	var destination = new google.maps.LatLng((days[day])[(days[day]).length-1].latitude, (days[day])[(days[day]).length-1].longitude);
+	var waypoints = [];
+	if (days[day].length > 2){
+		for(var i = 1; i < days[day].length - 1; i++){
+			var tuple = {};
+			tuple.stopover = true;
+			tuple.location = (new google.maps.LatLng((days[day])[i].latitude, (days[day])[i].longitude));
+			waypoints.push(tuple);
+		}
+	}
+	var request = {
+		origin:origin,
+		destination:destination,
+		waypoints:waypoints,
+		travelMode: google.maps.TravelMode.WALKING
+	};
+	directionsService.route(request, function(result, status) {
+		if (status == google.maps.DirectionsStatus.OK) {
+			directionsDisplay.setDirections(result);
+		}
+	}); 
+}
+
+$('.list-group-item').on('click',function(e){
+    var previous = $(this).closest(".list-group").children(".active");
+    previous.removeClass('active'); // previous list-item
+    $(e.target).addClass('active'); // activated list-item
+  });
 
 google.maps.event.addDomListener(window, 'load', initialize);
